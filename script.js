e6d8e427d6361c0022949241e17b5f86c5ba0370
MKCS2299 <98272921+MKCS2299@users.noreply.github.com>
2026-02-06 16:03:36 +0530

    script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-const elements = document.querySelectorAll('.fade-in');
-
-window.addEventListener('load', () => {
-  elements.forEach(el => {
-    el.classList.add('show');
-  });
-});
+console.log("JS LOADED");
